7eb716351d829a7028e3bd95ee7654f22ee18d20
ikeda-edash <t.ikeda@e-dash.io>
2024-07-09 15:35:55 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/t-ikedash/devcycle-go
+
+go 1.22
+
+require (
+	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/devcyclehq/go-server-sdk/v2 v2.16.1
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.18.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/launchdarkly/eventsource v1.7.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/matryer/try v0.0.0-20161228173917-9ac251b645a2 // indirect
+	github.com/open-feature/go-sdk v1.8.0 // indirect
+	github.com/twmb/murmur3 v1.1.7 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/exp v0.0.0-20230811145659-89c5cff77bcb // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/caarlos0/env"
+
+	devcycle "github.com/devcyclehq/go-server-sdk/v2"
+)
+
+type devCycle struct {
+	SDKKey    string `env:"DEVCYCLE_SERVER_SDK_KEY"`
+	SampleKey string `env:"SAMPLE_KEY"`
+}
+
+func main() {
+	cfg := &devCycle{}
+	if err := env.Parse(cfg); err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+	sdkKey := cfg.SDKKey
+
+	options := devcycle.Options{
+		EnableEdgeDB:                 false,
+		EnableCloudBucketing:         false,
+		EventFlushIntervalMS:         30 * time.Second,
+		ConfigPollingIntervalMS:      1 * time.Minute,
+		RequestTimeout:               30 * time.Second,
+		DisableAutomaticEventLogging: false,
+		DisableCustomEventLogging:    false,
+	}
+	fmt.Println(sdkKey)
+	devcycleClient, err := devcycle.NewClient(sdkKey, &options)
+	if err != nil {
+		log.Fatalf("Error initializing DevCycle client: %v", err)
+	}
+	user := devcycle.User{
+		UserId: "example_user_id",
+	}
+	key := cfg.SampleKey
+	variableValue, err := devcycleClient.VariableValue(user, key, false)
+	fmt.Printf("Variable value: %v\n", variableValue)
+	if variableValue.(bool) {
+		// Put feature code here, or launch feature from here
+		fmt.Println("Variable" + key + " is enabled")
+	} else {
+		// Put feature code here, or launch feature from here
+		fmt.Println("Variable " + key + " is disabled")
+	}
+}
